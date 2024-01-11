a846d870ad535bd9118071125ad1175d43b691bd
Tyler Coman <133472055+zhutyler21@users.noreply.github.com>
2024-01-11 12:51:31 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, request, send_file
+from PIL import Image
+import io
 
 app = Flask(__name__)
 
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    image = Image.open(file.stream).convert('L')
+    img_io = io.BytesIO()
+    image.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
 
-@app.route('/')
-def hello():
-    return 'Hello, world'
-
-
-@app.route('/test')
-def test():
-    return 'Test'
-
-@app.route('/result')
-def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
