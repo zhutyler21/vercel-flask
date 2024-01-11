2a66f3dc63032be6d08b359c3b960680a6eaeb85
zhutyler21 <zhutyler21@gmail.com>
2024-01-11 13:59:34 +0800

    博客展示

--- api/index.py
+++ api/index.py
@@ -1,19 +1,20 @@
-from flask import Flask, request, send_file
-from PIL import Image
-import io
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/upload_image', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        return 'No file part'
-    file = request.files['file']
-    image = Image.open(file.stream).convert('L')
-    img_io = io.BytesIO()
-    image.save(img_io, 'JPEG', quality=70)
-    img_io.seek(0)
-    return send_file(img_io, mimetype='image/jpeg')
+@app.route('/')
+def home():
+    # Get list of files in /static/img
+    img_files = os.listdir(os.path.join(app.root_path, 'static/img'))
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Filter out non-image file types if necessary
+    img_files = [img for img in img_files if img.endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+
+    # Select the first image file
+    img_file = img_files[0] if img_files else None
+
+    return render_template('index.html', img_file=img_file)
+
+if __name__ == '__main__':
+    app.run(debug=True)
